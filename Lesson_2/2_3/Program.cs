5be1c640a3c3a1f63077b018b4627a54b74e0a15
Viktor Golev <vwpiligrim@gmail.com>
2023-12-24 22:42:26 +0300

    Classwork task 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿ int firstNumber, secondNumber;
+
+            Console.Write("Enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine()!);
+
+            if (firstNumber % secondNumber == 0)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                int remainder = firstNumber % secondNumber;
+                Console.WriteLine($"No, {remainder}");
+            }

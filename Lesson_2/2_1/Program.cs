524c3b187f4344d9deefcc2a373e3274ac25db4b
Viktor Golev <vwpiligrim@gmail.com>
2023-12-24 16:23:58 +0300

    Classwork task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter a three-digit number: ");
+        int number = int.Parse(Console.ReadLine()!);
+        int result = (number / 100) * 10 + (number % 10);
+        Console.WriteLine("Result: " + result);

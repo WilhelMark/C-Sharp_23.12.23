5eed89424e7153e3dd7ed8179f91fafbd0bd0ebb
Viktor Golev <vwpiligrim@gmail.com>
2023-12-24 22:14:52 +0300

    Classwork task 2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Entering a three-digit number
+        Console.Write("Enter a three-digit number: ");
+        int number = Convert.ToInt32(Console.ReadLine()!);
+
+        // Extracting the second and third digits
+        int secondDigit = (number / 10) % 10;
+        int thirdDigit = number % 10;
+
+        // Elevation of the second digit to a degree equal to the third digit
+        int result = (int)Math.Pow(secondDigit, thirdDigit);
+
+        // Result output
+        Console.WriteLine($"{secondDigit}^{thirdDigit} = {result}");

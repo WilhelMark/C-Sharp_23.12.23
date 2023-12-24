ff7820f8c300ff297b226c1b719cb4601f1cbef2
Viktor Golev <vwpiligrim@gmail.com>
2023-12-24 13:57:53 +0300

    Rewritten, improved the code of Classwork task 1_1

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,3 +1,11 @@
-﻿Console.Write("Enter a number: ");
-int num1 = int.Parse(Console.ReadLine()!);
-Console.WriteLine(num1 * num1);
+﻿// Prompt the user to input the first and second numbers
+        Console.Write("Input the first number: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Input the second number: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        // Check if the first number is a square of the second number
+        bool isFirstNumberASquare = firstNumber * firstNumber == secondNumber;
+
+        // Print the result
+        Console.WriteLine("Is the first number a square of the second number? " + isFirstNumberASquare);

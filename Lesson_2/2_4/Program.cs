6163d5ffef98b7bc30cca472bcebc5b30f251bbc
Viktor Golev <vwpiligrim@gmail.com>
2023-12-24 22:55:43 +0300

    Classwork task 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 100)
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+        else
+        {
+            int thirdFromEnd = (number / 100) % 10;
+            Console.WriteLine("Третья цифра: " + thirdFromEnd);
+        }

889c7e269f25304c4b715356474057ac5f79a3d3
Viktor Golev <vwpiligrim@gmail.com>
2023-12-26 15:25:21 +0300

    Homework task 2_3

---
+++ Homework/2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = 0;
+    bool isValidInput = false;
+    while (!isValidInput) {
+      Console.WriteLine("Введите целое число от 10 до 99:");
+      string input = Console.ReadLine()!;
+      if (int.TryParse(input, out number) && number >= 10 && number <= 99) {
+        isValidInput = true;
+      } else {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число от 10 до 99.");
+      }
+    }
+    int maxDigit = 0;
+    while (number > 0) {
+      int digit = number % 10;
+      maxDigit = Math.Max(digit, maxDigit);
+      number = number / 10;
+    }
+    Console.WriteLine("Наибольшая цифра числа: " + maxDigit);

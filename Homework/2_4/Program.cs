ff34b3a87f112c8b5da354fa35fd1621375548e1
Viktor Golev <vwpiligrim@gmail.com>
2023-12-26 15:46:25 +0300

    Homework task 2_4

---
+++ Homework/2_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int n;
+            bool validInput = false;
+            do
+            {
+                Console.Write("Enter a natural number: ");
+                if (int.TryParse(Console.ReadLine()!, out n) && n > 0)
+                {
+                    validInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a natural number.");
+                }
+            } while (!validInput);
+
+            string digits = n.ToString();
+            for (int i = 0; i < digits.Length; i++)
+            {
+                Console.Write(digits[i]);
+                if (i < digits.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+            Console.WriteLine();

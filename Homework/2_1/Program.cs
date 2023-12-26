be091413145bb780b9aafd6a467acb883025db5b
Viktor Golev <vwpiligrim@gmail.com>
2023-12-26 12:36:54 +0300

    Homework task 2_1

---
+++ Homework/2_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine()!);
+
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }

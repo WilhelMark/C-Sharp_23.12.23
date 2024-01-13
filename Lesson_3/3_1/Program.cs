213af56531ce276b6a2ddf7f29c47a3d31142e98
Viktor Golev <vwpiligrim@gmail.com>
2024-01-13 09:39:59 +0300

    add Lesson3 Task 3_1

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] array = {1, 3, 4, 19, 3};
+        int numberToFind = 8;
+
+        bool isPresent = IsNumberPresent(array, numberToFind);
+
+        Console.WriteLine(isPresent ? "Да" : "Нет");
+    }
+
+    public static bool IsNumberPresent(int[] array, int number)
+    {
+        foreach (int element in array)
+        {
+            if (element == number)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}

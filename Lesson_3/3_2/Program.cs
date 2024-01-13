aa663fba6d35348d9e79f46277f0927a0e04a17a
Viktor Golev <vwpiligrim@gmail.com>
2024-01-13 09:58:50 +0300

    add Lesson3 Task 3_2

---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+         int[] array = new int[10] { -10, -5, 6, -1, 5, -6, 10, -3, 2, -4 };
+
+        // Замена отрицательных на положительные и наоборот
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = (array[i] < 0) ? -array[i] : array[i];
+        }
+
+        // Вывод результата
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}

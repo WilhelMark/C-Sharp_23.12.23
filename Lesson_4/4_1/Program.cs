f983afa15967798efc5c781845ce35285fa4f468
Viktor Golev <vwpiligrim@gmail.com>
2024-01-14 20:32:02 +0300

    add Homework: 3_1

--- Lesson_4/4_1/Program.cs
+++ Lesson_4/4_1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 
 class Program
 {
@@ -38,5 +39,85 @@ class Program
             }
         }
         return true;
+    }
+}
+*/
+using System;
+using System.Collections.Generic;
+
+class PrimeNumbersCounter
+{
+    // Метод для генерации случайного массива заданной длины
+    public static int[] GenerateRandomArray(int length)
+    {
+        Random random = new Random();
+        int[] numbers = new int[length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 100);
+        }
+        return numbers;
+    }
+
+    // Метод для подсчета количества простых чисел в массиве и вывода списка простых чисел
+    public static void CountPrimeNumbers(int[] numbers, out int count, out List<int> primes)
+    {
+        count = 0;
+        primes = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (IsPrime(number))
+            {
+                count++;
+                primes.Add(number);
+            }
+        }
+    }
+
+    // Метод для определения является ли число простым
+    public static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        for (int i = 2; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Метод для вывода результата на экран
+    // primes - список простых чисел в массиве
+    public static void PrintResult(int count, List<int> primes)
+    {
+        Console.WriteLine("Количество простых чисел в массиве: " + count);
+        Console.WriteLine("Список простых чисел в массиве: " + string.Join(", ", primes));
+    }
+}
+
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        int[] array;
+
+        if (args.Length >= 1)
+        {
+            // Преобразуем аргумент командной строки в целое число
+            int length = int.Parse(args[0]);
+            array = PrimeNumbersCounter.GenerateRandomArray(length);
+        }
+        else
+        {
+            array = PrimeNumbersCounter.GenerateRandomArray(10);
+        }
+
+        int count;
+        List<int> primes;
+        PrimeNumbersCounter.CountPrimeNumbers(array, out count, out primes);
+        PrimeNumbersCounter.PrintResult(count, primes);
     }
 }

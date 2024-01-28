ab48a2f9538654a0217ae42a902fb74c923c57fc
Viktor Golev <vwpiligrim@gmail.com>
2024-01-28 09:56:04 +0300

    add Lesson6: 6_2

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку:");
+        string inputString = Console.ReadLine()!; // ввод строки с клавиатуры
+
+        char[] charArray = new char[inputString.Length]; // создание массива символов
+
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            charArray[i] = inputString[i]; // заполнение массива символами строки
+        }
+
+        Console.WriteLine("Массив символов:");
+        foreach (char ch in charArray)
+        {
+            Console.Write(ch + " "); // вывод массива символов на экран
+        }
+    }
+}

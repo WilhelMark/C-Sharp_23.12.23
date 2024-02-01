8e74ce40cf890e4e94d95f10b25cbf31d86c6661
Viktor Golev <vwpiligrim@gmail.com>
2024-02-01 13:33:28 +0300

    add Lesson6: Homework 6_2

---
+++ Homework/6_2/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+string inputString = "LaTiN StRiNg";
+string outputString = "";
+
+foreach (char c in inputString)
+{
+    if (Char.IsUpper(c))
+    {
+        outputString += Char.ToLower(c);
+    }
+    else
+    {
+        outputString += c;
+    }
+}
+
+Console.WriteLine(outputString);
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку:"); // Просим пользователя ввести строку
+        string inputString = Console.ReadLine()!; // Читаем введенную пользователем строку
+        string outputString = "";
+
+        foreach (char c in inputString)
+        {
+            if (Char.IsUpper(c))
+            {
+                outputString += Char.ToLower(c);
+            }
+            else
+            {
+                outputString += c;
+            }
+        }
+
+        Console.WriteLine("Результат: " + outputString); // Выводим результат
+    }
+}

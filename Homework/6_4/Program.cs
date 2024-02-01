f9e6be950a18044b030d778ed9d1856a7e50fd23
Viktor Golev <vwpiligrim@gmail.com>
2024-02-01 15:52:02 +0300

    add Lesson6: Homework 6_4

---
+++ Homework/6_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Исходная строка
+        string input = "Задайте строку, состоящую из слов, разделенных пробелами";
+
+        // Разбиение исходной строки на слова
+        string[] words = input.Split(' ');
+
+        // Формирование строки в обратном порядке
+        Array.Reverse(words);
+        string reversedString = string.Join(" ", words);
+
+        // Вывод результата
+        Console.WriteLine(reversedString);
+    }
+}

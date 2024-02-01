c3209486f83dce980ec935f6f8a39923429f31e4
Viktor Golev <vwpiligrim@gmail.com>
2024-02-01 13:38:38 +0300

    add Lesson6: Homework 6_3

---
+++ Homework/6_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строку: ");
+        string input = Console.ReadLine()!;
+
+        if (IsPalindrome(input))
+        {
+            Console.WriteLine("Да, это палиндром.");
+        }
+        else
+        {
+            Console.WriteLine("Нет, это не палиндром.");
+        }
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        str = str.ToLower().Replace(" ", "");
+        for (int i = 0; i < str.Length / 2; i++)
+        {
+            if (str[i] != str[str.Length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}

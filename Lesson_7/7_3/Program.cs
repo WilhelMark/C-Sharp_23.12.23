4cc866eca6fa8437789d9c1bbef41f8768eea93e
Viktor Golev <vwpiligrim@gmail.com>
2024-02-04 16:30:28 +0300

    add Lesson7: 7_3

---
+++ Lesson_7/7_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    // Функция для проверки того, является ли символ согласным
+    static bool IsConsonant(char c)
+    {
+        return "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ".Contains(c);
+    }
+
+    // Рекурсивная функция для извлечения и отображения согласных из входной строки
+    static void ExtractConsonants(string input, int index)
+    {
+        // Базовый случай: если индекс равен длине строки, остановите рекурсию
+        if (index == input.Length)
+            return;
+
+        // Если символ в текущем индексе является согласным, отобразите его
+        if (IsConsonant(input[index]))
+            Console.Write(input[index] + " ");
+
+        // Рекурсивный вызов функции со следующим индексом
+        ExtractConsonants(input, index + 1);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите строку: ");
+        string input = Console.ReadLine()!;
+
+        // Вызов рекурсивной функции для извлечения и отображения согласных
+        ExtractConsonants(input, 0);
+    }
+}

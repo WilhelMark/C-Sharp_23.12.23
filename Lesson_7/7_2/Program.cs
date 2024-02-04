af87e870341811a5cd44fd03de9f9a6764e78d5c
Viktor Golev <vwpiligrim@gmail.com>
2024-02-04 16:22:11 +0300

    add Lesson7: 7_2

---
+++ Lesson_7/7_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число N: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        PrintNumbers(1, n); // вызов рекурсивной функции
+    }
+
+    static void PrintNumbers(int current, int n)
+    {
+        if (current > n) // базовый случай: если текущее число больше N, то выходим из функции
+        {
+            return;
+        }
+
+        Console.Write(current + " "); // выводим текущее число
+
+        PrintNumbers(current + 1, n); // рекурсивный вызов функции для следующего числа
+    }
+}

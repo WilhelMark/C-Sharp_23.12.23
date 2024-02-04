1991f50596e76227acaf80e6e878c7cc0b2c8c08
Viktor Golev <vwpiligrim@gmail.com>
2024-02-04 15:54:35 +0300

    add Lesson6: 7_1

---
+++ Lesson_7/7_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = SumOfDigits(number);
+            Console.WriteLine("The sum of the digits is: " + sum);
+        }
+
+        static int SumOfDigits(int n)
+        {
+            if (n / 10 == 0)
+            {
+                return n;
+            }
+            else
+            {
+                return n % 10 + SumOfDigits(n / 10);
+            }
+        }
+    }
+}

2ee3902110e31abdbffe291cae8e65a1e299c72f
Viktor Golev <vwpiligrim@gmail.com>
2024-02-07 14:04:56 +0300

    add Lesson7: Homework 7_2.2

---
+++ Homework/7_2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите M: ");
+        int m = int.Parse(Console.ReadLine()!);
+        Console.Write("Введите N: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine("Четные числа в промежутке от {0} до {1}:", m, n);
+        PrintEvenNumbers(m, n);
+    }
+
+    static void PrintEvenNumbers(int m, int n)
+    {
+        if (m > n)
+        {
+            return;
+        }
+
+        if (m % 2 == 0)
+        {
+            Console.Write(m + " ");
+        }
+
+        PrintEvenNumbers(m + 1, n);
+    }
+}
